b2afbe4f1268d414b56e59e56394607beb28182a
Matheus <matheus.percigo@escola.pr.gov.br>
2026-08-23 12:22:11 -0300

    yy

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-const lenis = new Lenis({
-  lerp: 0.1,
-  smoothWheel: true,
-  autoRaf: true,
-})
-
-lenis.on('scroll', ({ animatedScroll, velocity }) => {
-  console.log(animatedScroll, velocity)
-})
-
-// Initialize a new Lenis instance for smooth scrolling
-const lenis = new Lenis();
-
-// Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
-lenis.on('scroll', ScrollTrigger.update);
-
-// Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
-// This ensures Lenis's smooth scroll animation updates on each GSAP tick
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000); // Convert time from seconds to milliseconds
-});
-
-// Disable lag smoothing in GSAP to prevent any delay in scroll animations
-gsap.ticker.lagSmoothing(0);
+npm i lenis
+# or
+yarn add lenis
+# or
+pnpm add lenis
+import Lenis from 'lenis'
